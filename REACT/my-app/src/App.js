83adeb3ec6e5199e20d1db28b21a94f389020a04
marichka201184 <marichka201184@gmail.com>
2020-11-01 19:05:09 +0200

    done

--- REACT/my-app/src/App.js
+++ REACT/my-app/src/App.js
@@ -1,8 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
-import {todolist} from './data/data';
 import React from 'react';
-import Todo from './components/todo/todo';
 import Todolist from './components/todolist/todolist';
 
 function App() {
